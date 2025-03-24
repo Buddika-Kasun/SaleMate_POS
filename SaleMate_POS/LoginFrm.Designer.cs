7a2c2bf88eadc0307c6970e86479cf3bd458fa21
Buddika Kasun <buddikakasun80@gmail.com>
2025-03-24 13:06:48 +0530

    Login form design

--- SaleMate_POS/LoginFrm.Designer.cs
+++ SaleMate_POS/LoginFrm.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.usrNmLbl = new System.Windows.Forms.Label();
+            this.pwLbl = new System.Windows.Forms.Label();
+            this.userNmTxt = new System.Windows.Forms.TextBox();
+            this.pwTxt = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usrNmLbl
+            // 
+            this.usrNmLbl.AutoSize = true;
+            this.usrNmLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usrNmLbl.Location = new System.Drawing.Point(114, 140);
+            this.usrNmLbl.Name = "usrNmLbl";
+            this.usrNmLbl.Size = new System.Drawing.Size(144, 29);
+            this.usrNmLbl.TabIndex = 1;
+            this.usrNmLbl.Text = "User Name";
+            // 
+            // pwLbl
+            // 
+            this.pwLbl.AutoSize = true;
+            this.pwLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwLbl.Location = new System.Drawing.Point(114, 200);
+            this.pwLbl.Name = "pwLbl";
+            this.pwLbl.Size = new System.Drawing.Size(128, 29);
+            this.pwLbl.TabIndex = 2;
+            this.pwLbl.Text = "Password";
+            // 
+            // userNmTxt
+            // 
+            this.userNmTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNmTxt.Location = new System.Drawing.Point(317, 137);
+            this.userNmTxt.Name = "userNmTxt";
+            this.userNmTxt.Size = new System.Drawing.Size(298, 35);
+            this.userNmTxt.TabIndex = 3;
+            // 
+            // pwTxt
+            // 
+            this.pwTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwTxt.Location = new System.Drawing.Point(317, 197);
+            this.pwTxt.Name = "pwTxt";
+            this.pwTxt.Size = new System.Drawing.Size(298, 35);
+            this.pwTxt.TabIndex = 4;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.Location = new System.Drawing.Point(317, 303);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(151, 49);
+            this.loginBtn.TabIndex = 5;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            // 
+            // LoginFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(741, 450);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.pwTxt);
+            this.Controls.Add(this.userNmTxt);
+            this.Controls.Add(this.pwLbl);
+            this.Controls.Add(this.usrNmLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "LoginFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label usrNmLbl;
+        private System.Windows.Forms.Label pwLbl;
+        private System.Windows.Forms.TextBox userNmTxt;
+        private System.Windows.Forms.TextBox pwTxt;
+        private System.Windows.Forms.Button loginBtn;
     }
 }
 
